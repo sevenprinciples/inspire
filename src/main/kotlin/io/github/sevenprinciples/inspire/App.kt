a43c87a15909b24ca5af045fe9ed8c11cc7091a1
Romero, Juan, SEVEN PRINCIPLES <juan.romero@7p-group.com>
2017-07-02 21:02:12 +0200

    handling exceptions

--- src/main/kotlin/io/github/sevenprinciples/inspire/App.kt
+++ src/main/kotlin/io/github/sevenprinciples/inspire/App.kt
@@ -6,6 +6,7 @@ import com.mashape.unirest.http.Unirest
 import io.github.vyo.twig.logger.Level
 import io.github.vyo.twig.logger.Logger
 import spark.Spark.*
+import java.awt.PageAttributes
 import java.io.File
 import java.nio.file.Files
 import java.nio.file.Path
@@ -15,6 +16,9 @@ import java.util.*
 /**
  * Created by juan.romero on 02.07.17.
  */
+
+val MATTERMOST_TOKEN = System.getenv("MATTERMOST_TOKEN")
+val BASE_PATH = System.getenv("BASE_PATH")
 
 fun getQuotes(path: String): List<String>  {
     val uri = App.javaClass.classLoader.getResource(path).toURI()
@@ -40,13 +44,33 @@ object App {
         staticFileLocation("/public")
         logger.level = Level.DEBUG
 
+        before { request, _ ->
+            if (request.queryParams("token") != MATTERMOST_TOKEN) {
+                throw Exception("forbidden")
+            }
+        }
         before { request, _ -> logger.debug("request: $request") }
         after { _, response -> logger.debug("response: $response") }
 
+        exception(Exception::class.java, { exc, req, res ->
+
+            if ( exc.message == "forbidden") {
+                res.body("forbidden")
+                res.status(403)
+            } else {
+                res.body("internal server error")
+                res.status(500)
+                logger.error(exc)
+            }
+
+        })
+
         post("/api/inspire", { req, res ->
 
+            res.type("application/json")
+
             MattermostPost("${chooseQuote(regularQuotes)}${System.lineSeparator()}![](${Unirest.get("http://inspirobot.me/api?generate=true").asString().body})",
-                    "http://localhost:4567/static/inspirobot.png",
+                    "$BASE_PATH/static/inspirobot.png",
                     "in_channel")
 
         }, { obj -> gson.toJson(obj) })
